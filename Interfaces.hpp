f044ce2451b3d0dce7360a2ef5de9a16b62d3960
Janav Saluja <janavsaluja@ufl.edu>
2025-11-09 13:17:32 -0500

    LLS Big FIve completed

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -18,18 +18,12 @@ class QueueInterface {
     void virtual enqueue(const T& item) = 0;
     T virtual dequeue() = 0;
     T virtual peek() const = 0;
-    std::size_t virtual getSize() const noexcept = 0;
+    std::size_t virtualgetSize() const noexcept = 0;
 };
 
 
 template <typename T>
 class DequeInterface {
-    void virtual pushFront(const T& item) = 0;
-    void virtual pushBack(const T& item) = 0;
-    T virtual popFront() = 0;
-    T virtual popBack() = 0;
-    const T& virtual front() const = 0;
-    const T& virtual back() const = 0;
-    std::size_t virtual getSize() const noexcept = 0;
+
 };
 
