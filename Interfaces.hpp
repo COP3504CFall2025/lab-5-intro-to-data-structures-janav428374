7a571addc49831199a30f4ff47bd952de4e38d24
Janav Saluja <janavsaluja@ufl.edu>
2025-11-09 00:43:13 -0500

    LLS completed

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -6,7 +6,10 @@
 
 template <typename T>
 class StackInterface {
-
+    void push(const T& item) = 0;
+    T pop() = 0;
+    T peek() const = 0;
+    std::size_t getSize() const noexcept = 0;
 };
 
 
--- LLS.hpp
+++ LLS.hpp
@@ -4,6 +4,7 @@
 #include "LinkedList.hpp"
 #include <stdlib.h>
 #include <stdexcept>
+#include <utility>
 
 template <typename T>
 class LLS : public StackInterface<T> {
@@ -11,17 +12,61 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() {}
 
+    //Copy Constructor
+    LLS(const LLS<T>& other) {
+        list = other.getLinkedList();
+    }
+
+    //Move Constructor
+    LLS(const LLS<T>&& other) noexcept{
+        list = std::move(other.getLinkedList());
+    }
+
+    //Copy Assignment
+    LLS& operator=(const LLS<T>& rhs) {
+        list = rhs.getLinkedList();
+        return *this;
+    }
+
+    //Move Assignment
+    LLS& operator=(const LLS<T>&& rhs) noexcept{
+        list = std::move(rhs.getLinkedList());
+        return *this;
+    }
+
+    LinkedList<T>& getLinkedList() const {
+        return list;
+    }
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override {
+        list.addHead(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override {
+        T head_value = *(list.getHead());
+        list.removeHead();
+        return head_value;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        T head_value = *(list.getHead());
+        return head_value;
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    }
+
+    void PrintForward() {
+        list.printForward();
+    }
+
+    void PrintReverse() {
+        list.printNodeF();
+    }
 };
